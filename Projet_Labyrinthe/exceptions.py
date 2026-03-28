f9ecbdba81e51f63a0476331e8017912c73b340c
Manylos21 <manylbouibede.37@gmail.com>
2026-03-28 19:39:02 +0100

    Update exceptions.py

--- Projet_Labyrinthe/exceptions.py
+++ Projet_Labyrinthe/exceptions.py
@@ -7,3 +7,9 @@ class MouvementInvalideError(Exception):
     """
     def __init__(self, message="Mouvement invalide : vous avez percuté un mur."):
         super().__init__(message)
+
+class PiegeError(Exception):
+    """
+    Exception personnalisée levée quand le joueur tombe dans un piège.
+    """
+    pass
