080891f4617e5f0626ba5abfe5268656355f6a9b
Manylos21 <manylbouibede.37@gmail.com>
2026-03-30 04:28:40 +0200

    Update utils_fichiers.py

--- Projet_Labyrinthe/utils_fichiers.py
+++ Projet_Labyrinthe/utils_fichiers.py
@@ -1,14 +1,12 @@
-# utils_fichiers.py
-
 import datetime
 import os
 
 def loguer_erreur(exception):
-    """Enregistre les erreurs avec horodatage dans journal.log."""
+    # Enregistre les erreurs dans journal.log
     os.makedirs("data", exist_ok=True)
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     
-    with open("data/journal.log", "a", encoding="utf-8") as f:
+    with open("data/journal.log", "a") as f:
         f.write(f"[{timestamp}] ERREUR ({type(exception).__name__}) : {str(exception)}\n")
 
 def sauvegarder_labyrinthe(grille, nom_fichier="data/dernier_labyrinthe.txt"):
